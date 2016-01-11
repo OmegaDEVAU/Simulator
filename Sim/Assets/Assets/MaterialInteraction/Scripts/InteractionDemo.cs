435401ab5e55e388339c43ab44dd762143767083
OmegaDEVAU <omegadev.au@gmail.com>
2016-01-12 03:45:12 +1100

    2nd Upload
    12/1/16 3:40am - Re Upload Working uNet @ SQL server

--- Sim/Assets/Assets/MaterialInteraction/Scripts/InteractionDemo.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public class InteractionDemo : MonoBehaviour {
-
-	void OnCollisionEnter(Collision collision)
-	{
-		// Ask the system to spawn an effect based on the collision and the collider's physic material.
-		MaterialInteractionSystem.SpawnEffect(collision, GetComponent<Collider>().sharedMaterial);
-	}
-}
